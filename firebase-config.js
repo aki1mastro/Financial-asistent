6ba999fc4931be414db65a9d81810469b00c8564
aki1mastro <aki1mdauletkeldy@gmail.com>
2026-05-20 16:53:43 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD9DdWpxv8OKmF6hpuNwDlLlfpbOx3riAE",
   authDomain: "financial-asistent.firebaseapp.com",
@@ -8,3 +7,5 @@ const firebaseConfig = {
   appId: "1:702605526775:web:150d4038df8aa053dfb5f2",
   measurementId: "G-H10XED4219"
 };
+
+export default firebaseConfig;
