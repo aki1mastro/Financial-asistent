2153747a22b2b1afabddf945eedf902aa8a34c1a
aki1mastro <aki1mdauletkeldy@gmail.com>
2026-05-20 16:50:46 +0500

    Clean up firebase-config.js by removing unused code
    Removed Firebase SDK imports and initialization code.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD9DdWpxv8OKmF6hpuNwDlLlfpbOx3riAE",
@@ -15,7 +8,3 @@ const firebaseConfig = {
   appId: "1:702605526775:web:150d4038df8aa053dfb5f2",
   measurementId: "G-H10XED4219"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
